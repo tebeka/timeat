d897a1c9f26d11c12f3656481e768ed21442415e
Miki Tebeka <miki.tebeka@gmail.com>
2014-05-24 08:48:17 +0300

    usage doc

--- timeat.go
+++ timeat.go
@@ -1,4 +1,18 @@
-// Print current time at address using Google Maps API
+/*
+Print current time at address using Google Maps API
+
+Example Usage
+
+    $ ./timeat 'los angeles'
+    Los Angeles, CA, USA: Fri May 23, 2014 22:33
+    $ ./timeat paris
+    Paris, France: Sat May 24, 2014 07:37
+    Paris, TX, USA: Sat May 24, 2014 00:37
+    Paris, TN 38242, USA: Sat May 24, 2014 00:37
+    Paris, IL 61944, USA: Sat May 24, 2014 00:37
+    Paris, KY 40361, USA: Sat May 24, 2014 01:37
+    $
+*/
 package main
 
 import (
